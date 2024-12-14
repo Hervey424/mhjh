d70ce4b9eefc4ca8159b1dbc40993335e59f2fb7
xiaowei-pc-a415 <619608182@qq.com>
2024-12-14 11:34:38 +0800

    1214-1134

--- SamiraFight.js
+++ SamiraFight.js
@@ -2,7 +2,7 @@ var SamiraFight = (function () {
   function SamiraFight() {}
   __class(SamiraFight, 'com.modules.map.model.auto.SamiraFight');
 
-  SamiraFight.version = '1211-1256';
+  SamiraFight.version = '1214-1134';
   SamiraFight.isInit = false;
   SamiraFight.personId = '';
   SamiraFight.autoOpenTimer = 0;
@@ -4227,7 +4227,7 @@ var SamiraFight = (function () {
     S2C_MyPlayerInfoMessage,
     GameHandler.create(this, cmd => {
       const name = cmd.name;
-      if ((!['项小伟', '海莲的乐儿', '阳光的夏天', '绿色的思念', '冷丶风', '沙场学霸', '元风涵容', '费博赡', '若男', '听话的珊珊', '傲慢的新儿', '刚炮'].includes(name)) && (!name.includes('元风涵容'))) {
+      if ((!['项小伟', '阳光的夏天', '绿色的思念', '冷丶风', '沙场学霸', '若男', '听话的珊珊', '傲慢的新儿', '刚炮'].includes(name)) && (!name.includes('听话的珊珊')) && (!name.includes('元风涵容'))) {
         alert('未开启!');
         return;
       }
